b1be18743595ce524651b77ead0fc0847960608c
Diego Esmerio <diego.esmerio@fitsolucoes.biz>
2016-09-05 17:53:24 -0300

    Foreign Key support (unfinished)

---
+++ TestTableFill/ForeignCreator.fs
@@ -0,0 +1,16 @@
+﻿module ForeignCreator
+open Definitions
+
+let create insertCreator tables =
+    let namedMap =
+        tables |> List.map (fun t -> t.Name,t) |> Map.ofSeq
+    let rec innerCreate (ins, key) tab =
+        let dIns, dKey =
+            tab.Columns
+            |> List.filter (fun c-> c.IsForeign)
+            |> List.map (fun c->c.GetForeign |> fst) |> Set.ofList |> Seq.map (fun n -> namedMap |> Map.find n)
+            |> Seq.fold innerCreate (ins,key)
+        let nIns, nKey = insertCreator tab dKey
+        nIns::dIns,nKey
+    tables
+    |> List.fold innerCreate ([], Map.empty)
